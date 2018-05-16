0b25429d9e46e74a1747a8ef5f1037b5f9f3f877
Daniel Wood <daniel.j.wood11@gmail.com>
2018-05-16 15:10:26 -0400

    fix n/a superscripts, tighten super styles]

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ var IS_MOBILE;
 var IS_PHONE;
 var IS_PHONESM;
 var BREAKS ={"perc_debt_collect":[0.22, .31, .39, .49],"med_debt_collect":[1200, 1500, 1800, 2300], "perc_debt_med":[.11,.18,.26,.34], "med_debt_med":[500,700,950,1250], "perc_pop_nw":[.13,.28,.46,.67], "perc_pop_no_ins":[.08,.13,.18,.26], "avg_income":[52650,63850,77900,101050],"perc_stud_debt":[0.10,0.13,0.16,0.20],"med_stud_debt": [12550,15050,17450,20350],"perc_stud_debt_collect": [0.01,0.02,0.03,0.06],"perc_stud_debt_collect_STUD": [0.07,0.13,0.2,0.3],"med_stud_debt_collect": [6150,7550,9000,10700],"med_mon_pmt": [135,155,175,195],"perc_no_bach": [0.59,0.71,0.79,0.85]};
-var legendWidth = {"perc_debt_collect": 60,"perc_debt_med": 58,"med_debt_collect": 73,"med_debt_med": 70,"perc_pop_nw": 63,"perc_pop_no_ins": 60,"avg_income": 89,"perc_stud_debt":89,"med_stud_debt":89,"perc_stud_debt_collect":89,"perc_stud_debt_collect_STUD":89,"med_stud_debt_collect":89,"med_mon_pmt":89,"perc_no_bach":89};
+var legendWidth = {"perc_debt_collect": 60,"perc_debt_med": 58,"med_debt_collect": 73,"med_debt_med": 70,"perc_pop_nw": 63,"perc_pop_no_ins": 60,"avg_income": 89,"perc_stud_debt":60,"med_stud_debt":89,"perc_stud_debt_collect":60,"perc_stud_debt_collect_STUD":60,"med_stud_debt_collect":89,"med_mon_pmt":70,"perc_no_bach":60};
 
 // var legendTranslate = {"perc_debt_collect": width-60, "perc_debt_med": 644, "med_debt_collect": 628, "med_debt_med":631, "perc_pop_nw":638, "perc_pop_no_ins": 642, "avg_income":615}
 
@@ -457,20 +457,20 @@ function ready(error, us, county, state, county2, state2) {
     .range(["#cfe8f3", "#73bfe2", "#1696d2", "#0a4c6a", "#000000"])  
 
  /*ADD DROPDOWNS*/
-    var categoryData = [{label: "Share with any debt in collections <i>ᵃ</i>", variable: "perc_debt_collect"},
-    {label: "Median debt in collections <i>ᵃ</i>", variable: "med_debt_collect"},
-    {label: "Share with medical debt in collections <i>ᵃ</i>", variable: "perc_debt_med"},
-    {label: "Median medical debt in collections <i>ᵃ</i>", variable: "med_debt_med"},
+    var categoryData = [{label: "Share with any debt in collections<i>ᵃ</i>", variable: "perc_debt_collect"},
+    {label: "Median debt in collections<i>ᵃ</i>", variable: "med_debt_collect"},
+    {label: "Share with medical debt in collections<i>ᵃ</i>", variable: "perc_debt_med"},
+    {label: "Median medical debt in collections<i>ᵃ</i>", variable: "med_debt_med"},
     {label: "Nonwhite population share", variable: "perc_pop_nw"},
     {label: "Share without health insurance coverage", variable: "perc_pop_no_ins" },
     {label: "Average household income", variable: "avg_income"}]
     
-    var categoryData2 = [{label: "Share with student loan debt <i>ᵃ</i>", variable: "perc_stud_debt"},
-    {label: "Median student loan debt <i>ᵃ</i>", variable: "med_stud_debt"},
-    {label: "Share of student loan holders with student loan debt in collections <i>ᵃ ᵈ</i>", variable: "perc_stud_debt_collect_STUD"},    
-    {label: "Median student loan debt in collections <i>ᵃ</i>", variable: "med_stud_debt_collect"},
-    {label: "Median monthly student loan payment <i>ᵃ</i>", variable: "med_mon_pmt"},
-    {label: "Share of people with credit records who have student loan debt in collections <i>ᵃ ᵉ</i>", variable: "perc_stud_debt_collect"},
+    var categoryData2 = [{label: "Share with student loan debt<i>ᵃ</i>", variable: "perc_stud_debt"},
+    {label: "Median student loan debt<i>ᵃ</i>", variable: "med_stud_debt"},
+    {label: "Share of student loan holders with student loan debt in collections<i>ᵃ ᵈ</i>", variable: "perc_stud_debt_collect_STUD"},    
+    {label: "Median student loan debt in collections<i>ᵃ</i>", variable: "med_stud_debt_collect"},
+    {label: "Median monthly student loan payment<i>ᵃ</i>", variable: "med_mon_pmt"},
+    {label: "Share of people with credit records who have student loan debt in collections<i>ᵃ ᵉ</i>", variable: "perc_stud_debt_collect"},
     {label: "Nonwhite population share", variable: "perc_pop_nw"},
     {label: "Share without a bachelor’s degree", variable: "perc_no_bach"},
     {label: "Average household income", variable: "avg_income" }]
@@ -539,7 +539,6 @@ function ready(error, us, county, state, county2, state2) {
       optionsCategory.enter()
         .append('option')
         .html(function(d) {
-          // console.log(d.label)          
           return d.label          
         })
         .attr('value', function(d) {
@@ -1174,15 +1173,15 @@ function ready(error, us, county, state, county2, state2) {
     
     if (type) {
       if (type == "medical") {
-        var groups = ["Share with any debt in collections <span class=\"annotation\"><sup>a</sup></span>", "Median debt in collections <span class=\"annotation\"><sup>a</sup></span>", "Share with medical debt in collections <span class=\"annotation\"><sup>a</sup></span>", "Median medical debt in collections <span class=\"annotation\"><sup>a</sup></span>","Nonwhite population share", "Share without health insurance coverage","Average household income"]
+        var groups = ["Share with any debt in collections<span class=\"annotation\"><sup>a</sup></span>", "Median debt in collections<span class=\"annotation\"><sup>a</sup></span>", "Share with medical debt in collections<span class=\"annotation\"><sup>a</sup></span>", "Median medical debt in collections<span class=\"annotation\"><sup>a</sup></span>","Nonwhite population share", "Share without health insurance coverage","Average household income"]
         var rowData = ["perc_debt_collect", "med_debt_collect", "perc_debt_med", "med_debt_med", "perc_pop_nw", "perc_pop_no_ins", "avg_income"]    
       } else if (type == "student") {
-        var groups = [ "Share with student loan debt <span class=\"annotation\"><sup>a</sup></span>","Median student loan debt <span class=\"annotation\"><sup>a</sup></span>","Share of student loan holders with student loan debt in collections <span class=\"annotation\"><sup>a</sup> <sup>d</sup></span>","Median student loan debt in collections <span class=\"annotation\"><sup>a</sup></span>","Median monthly student loan payment <span class=\"annotation\"><sup>a</sup></span>","Share of people with credit records who have student loan debt in collections <span class=\"annotation\"><sup>a</sup> <sup>e</sup></span>","Nonwhite population share","Share without a bachelor’s degree","Average household income"];
+        var groups = [ "Share with student loan debt<span class=\"annotation\"><sup>a</sup></span>","Median student loan debt<span class=\"annotation\"><sup>a</sup></span>","Share of student loan holders with student loan debt in collections<span class=\"annotation\"><sup>a</sup> <sup>d</sup></span>","Median student loan debt in collections<span class=\"annotation\"><sup>a</sup></span>","Median monthly student loan payment<span class=\"annotation\"><sup>a</sup></span>","Share of people with credit records who have student loan debt in collections<span class=\"annotation\"><sup>a</sup> <sup>e</sup></span>","Nonwhite population share","Share without a bachelor’s degree","Average household income"];
         var rowData = ["perc_stud_debt","med_stud_debt","perc_stud_debt_collect_STUD","med_stud_debt_collect","med_mon_pmt","perc_stud_debt_collect","perc_pop_nw","perc_no_bach","avg_income"]    
       }
     }
 
-    // var groups = ["Share with any debt in collections <span class=\"annotation\"><sup>a</sup></span>", "Median debt in collections <span class=\"annotation\"><sup>a</sup></span>", "Share with medical debt in collections <span class=\"annotation\"><sup>a</sup></span>", "Median medical debt in collections <span class=\"annotation\"><sup>a</sup></span>","Nonwhite population share", "Share without health insurance coverage","Average household income"]
+    // var groups = ["Share with any debt in collections<span class=\"annotation\"><sup>a</sup></span>", "Median debt in collections<span class=\"annotation\"><sup>a</sup></span>", "Share with medical debt in collections<span class=\"annotation\"><sup>a</sup></span>", "Median medical debt in collections<span class=\"annotation\"><sup>a</sup></span>","Nonwhite population share", "Share without health insurance coverage","Average household income"]
     
     // var rowData = ["perc_debt_collect", "med_debt_collect", "perc_debt_med", "med_debt_med", "perc_pop_nw", "perc_pop_no_ins", "avg_income"]
     var table = d3.select("#table-div")
@@ -1866,9 +1865,9 @@ function ready(error, us, county, state, county2, state2) {
             .html(function(d) { 
               var parentClass = $(this).closest(".rect-g").attr("class")
               if (parentClass.search("All") > -1) { 
-                var noData = (d[variable] == "n<50") ? "n/a<sup><i>b</i></sup>" : "n/a<sup><i>c</i></sup>"
-                var noData_wh = (d[WHITE_ph] == "n<50") ? "n/a<sup><i>b</i></sup>" : "n/a<sup><i>c</i></sup>"
-                var noData_nw = (d[NONWHITE_ph] == "n<50") ? "n/a<sup><i>b</i></sup>" : "n/a<sup><i>c</i></sup>"
+                var noData = (d[variable] == "n<50") ? "n/a<tspan font-style='italic'  baseline-shift='super'>b</tspan>" : "n/a<tspan font-style='italic'  baseline-shift='super'>c</tspan>"
+                var noData_wh = (d[WHITE_ph] == "n<50") ? "n/a<tspan font-style='italic'  baseline-shift='super'>b</tspan>" : "n/a<tspan font-style='italic'  baseline-shift='super'>c</tspan>"
+                var noData_nw = (d[NONWHITE_ph] == "n<50") ? "n/a<tspan font-style='italic'  baseline-shift='super'>b</tspan>" : "n/a<tspan font-style='italic'  baseline-shift='super'>c</tspan>"
                 if (d[NONWHITE_ph] == "n<50" || (d[WHITE_ph]) == "n<50" || (d[variable]) == "n<50") { 
                   d3.select("#notes-section > p.note1").style("opacity", 1)
                 }
@@ -1915,9 +1914,9 @@ function ready(error, us, county, state, county2, state2) {
               }
             })
             .html(function(d) { 
-              var noData = (d[variable] == "n<50") ? "n/a<sup><i>b</i></sup>" : "n/a<sup><i>c</i></sup>"
-              var noData_wh = (d[WHITE_ph] == "n<50") ? "n/a<sup><i>b</i></sup>" : "n/a<sup><i>c</i></sup>"
-              var noData_nw = (d[NONWHITE_ph] == "n<50") ? "n/a<sup><i>b</i></sup>" : "n/a<sup><i>c</i></sup>"
+              var noData = (d[variable] == "n<50") ? "n/a<tspan font-style='italic'  baseline-shift='super'>b</tspan>" : "n/a<tspan font-style='italic'  baseline-shift='super'>c</tspan>"
+              var noData_wh = (d[WHITE_ph] == "n<50") ? "n/a<tspan font-style='italic'  baseline-shift='super'>b</tspan>" : "n/a<tspan font-style='italic'  baseline-shift='super'>c</tspan>"
+              var noData_nw = (d[NONWHITE_ph] == "n<50") ? "n/a<tspan font-style='italic'  baseline-shift='super'>b</tspan>" : "n/a<tspan font-style='italic'  baseline-shift='super'>c</tspan>"
               var parentClass = $(this).closest(".rect-g").attr("class")
               if (d[NONWHITE_ph] == "n<50" || (d[WHITE_ph]) == "n<50" || (d[variable]) == "n<50") { 
                 d3.select("#notes-section > p.note1").style("opacity", 1)
@@ -1967,9 +1966,10 @@ function ready(error, us, county, state, county2, state2) {
                 }
               })
               .html(function(d) { 
-                var noData = (d[variable] == "n<50") ? "n/a<sup><i>b</i></sup>" : "n/a<sup><i>c</i></sup>"
-                var noData_wh = (d[WHITE_ph] == "n<50") ? "n/a<sup><i>b</i></sup>" : "n/a<sup><i>c</i></sup>"
-                var noData_nw = (d[NONWHITE_ph] == "n<50") ? "n/a<sup><i>b</i></sup>" : "n/a<sup><i>c</i></sup>"
+                var noData = (d[variable] == "n<50") ? "n/a<tspan font-style='italic'  baseline-shift='super'>b</tspan>" : "n/a<tspan font-style='italic'  baseline-shift='super'>c</tspan>"
+                var noData_wh = (d[WHITE_ph] == "n<50") ? "n/a<tspan font-style='italic'  baseline-shift='super'>b</tspan>" : "n/a<tspan font-style='italic'  baseline-shift='super'>c</tspan>"
+                var noData_nw = (d[NONWHITE_ph] == "n<50") ? "n/a<tspan font-style='italic'  baseline-shift='super'>b</tspan>" : "n/a<tspan font-style='italic'  baseline-shift='super'>c</tspan>"
+
                 if (d[NONWHITE_ph] == "n<50" || (d[WHITE_ph]) == "n<50" || (d[variable]) == "n<50") { 
                   d3.select("#notes-section > p.note1").style("opacity", 1)
                 }
@@ -1984,6 +1984,7 @@ function ready(error, us, county, state, county2, state2) {
                 }else{
                   return (isNaN(d[WHITE_ph]) != true) ? formatNumber(d[WHITE_ph]) : noData_wh
                 }
+                // return "<tspan font-style='italic'>fart</tspan>"
               })
           })
       }
@@ -2062,9 +2063,9 @@ function ready(error, us, county, state, county2, state2) {
                 }
               })
             .html(function(d) { 
-              var noData = (d[variable] == "n<50") ? "n/a<sup><i>b</i></sup>" : "n/a<sup><i>c</i></sup>"
-              var noData_wh = (d[WHITE] == "n<50") ? "n/a<sup><i>b</i></sup>" : "n/a<sup><i>c</i></sup>"
-              var noData_nw = (d[NONWHITE] == "n<50") ? "n/a<sup><i>b</i></sup>" : "n/a<sup><i>c</i></sup>"
+              var noData = (d[variable] == "n<50") ? "n/a<tspan font-style='italic'  baseline-shift='super'>b</tspan>" : "n/a<tspan font-style='italic'  baseline-shift='super'>c</tspan>"
+              var noData_wh = (d[WHITE] == "n<50") ? "n/a<tspan font-style='italic'  baseline-shift='super'>b</tspan>" : "n/a<tspan font-style='italic'  baseline-shift='super'>c</tspan>"
+              var noData_nw = (d[NONWHITE] == "n<50") ? "n/a<tspan font-style='italic'  baseline-shift='super'>b</tspan>" : "n/a<tspan font-style='italic'  baseline-shift='super'>c</tspan>"
               var parentClass = d3.select(this.parentNode).attr('class');
               if (parentClass.search("All") > -1) {
                 return (isNaN(d[variable]) != true) ? formatNumber(d[variable]) : noData
@@ -2128,9 +2129,9 @@ function ready(error, us, county, state, county2, state2) {
                 }
               })
               .html(function(d) { 
-                var noData = (d[variable] == "n<50") ? "n/a<sup><i>b</i></sup>" : "n/a<sup><i>c</i></sup>"
-                var noData_wh = (d[WHITE] == "n<50") ? "n/a<sup><i>b</i></sup>" : "n/a<sup><i>c</i></sup>"
-                var noData_nw = (d[NONWHITE] == "n<50") ? "n/a<sup><i>b</i></sup>" : "n/a<sup><i>c</i></sup>"
+                var noData = (d[variable] == "n<50") ? "n/a<tspan font-style='italic'  baseline-shift='super'>b</tspan>" : "n/a<tspan font-style='italic'  baseline-shift='super'>c</tspan>"
+                var noData_wh = (d[WHITE] == "n<50") ? "n/a<tspan font-style='italic'  baseline-shift='super'>b</tspan>" : "n/a<tspan font-style='italic'  baseline-shift='super'>c</tspan>"
+                var noData_nw = (d[NONWHITE] == "n<50") ? "n/a<tspan font-style='italic'  baseline-shift='super'>b</tspan>" : "n/a<tspan font-style='italic'  baseline-shift='super'>c</tspan>"
                 var parentClass = d3.select(this.parentNode).attr('class');
                 if (parentClass.search("All") > -1) {
                   return (isNaN(d[variable]) != true) ? formatNumber(d[variable]) : noData
@@ -2198,9 +2199,9 @@ function ready(error, us, county, state, county2, state2) {
                   }
                 })
                 .html(function(d) { 
-                  var noData = (d[variable] == "n<50") ? "n/a<sup><i>b</i></sup>" : "n/a<sup><i>c</i></sup>"
-                  var noData_wh = (d[WHITE] == "n<50") ? "n/a<sup><i>b</i></sup>" : "n/a<sup><i>c</i></sup>"
-                  var noData_nw = (d[NONWHITE] == "n<50") ? "n/a<sup><i>b</i></sup>" : "n/a<sup><i>c</i></sup>"
+                  var noData = (d[variable] == "n<50") ? "n/a<tspan font-style='italic'  baseline-shift='super'>b</tspan>" : "n/a<tspan font-style='italic'  baseline-shift='super'>c</tspan>"
+                  var noData_wh = (d[WHITE] == "n<50") ? "n/a<tspan font-style='italic'  baseline-shift='super'>b</tspan>" : "n/a<tspan font-style='italic'  baseline-shift='super'>c</tspan>"
+                  var noData_nw = (d[NONWHITE] == "n<50") ? "n/a<tspan font-style='italic'  baseline-shift='super'>b</tspan>" : "n/a<tspan font-style='italic'  baseline-shift='super'>c</tspan>"
                   var parentClass = d3.select(this.parentNode).attr('class');
                   if (parentClass.search("All") > -1) {
                     return (isNaN(d[variable]) != true) ? formatNumber(d[variable]) : noData
@@ -2302,10 +2303,10 @@ function ready(error, us, county, state, county2, state2) {
 
     if (type) {
       if (type == "medical") {
-        var groups = ["Share with any debt in collections <span class=\"annotation\"><sup>a</sup></span>", "Median debt in collections <span class=\"annotation\"><sup>a</sup></span>", "Share with medical debt in collections <span class=\"annotation\"><sup>a</sup></span>", "Median medical debt in collections <span class=\"annotation\"><sup>a</sup></span>","Nonwhite population share", "Share without health insurance coverage","Average household income"]
+        var groups = ["Share with any debt in collections<span class=\"annotation\"><sup>a</sup></span>", "Median debt in collections<span class=\"annotation\"><sup>a</sup></span>", "Share with medical debt in collections<span class=\"annotation\"><sup>a</sup></span>", "Median medical debt in collections<span class=\"annotation\"><sup>a</sup></span>","Nonwhite population share", "Share without health insurance coverage","Average household income"]
         var rowData = ["perc_debt_collect", "med_debt_collect", "perc_debt_med", "med_debt_med", "perc_pop_nw", "perc_pop_no_ins", "avg_income"]    
       } else if (type == "student") {
-        var groups = [ "Share with student loan debt <span class=\"annotation\"><sup>a</sup></span>","Median student loan debt <span class=\"annotation\"><sup>a</sup></span>","Share of student loan holders with student loan debt in collections <span class=\"annotation\"><sup>a</sup> <sup>d</sup></span>","Median student loan debt in collections <span class=\"annotation\"><sup>a</sup></span>","Median monthly student loan payment <span class=\"annotation\"><sup>a</sup></span>","Share of people with credit records who have student loan debt in collections <span class=\"annotation\"><sup>a</sup> <sup>e</sup></span>","Nonwhite population share","Share without a bachelor’s degree","Average household income"];
+        var groups = [ "Share with student loan debt<span class=\"annotation\"><sup>a</sup></span>","Median student loan debt<span class=\"annotation\"><sup>a</sup></span>","Share of student loan holders with student loan debt in collections<span class=\"annotation\"><sup>a</sup> <sup>d</sup></span>","Median student loan debt in collections<span class=\"annotation\"><sup>a</sup></span>","Median monthly student loan payment<span class=\"annotation\"><sup>a</sup></span>","Share of people with credit records who have student loan debt in collections<span class=\"annotation\"><sup>a</sup> <sup>e</sup></span>","Nonwhite population share","Share without a bachelor’s degree","Average household income"];
         var rowData = ["perc_stud_debt","med_stud_debt","perc_stud_debt_collect_STUD","med_stud_debt_collect","med_mon_pmt","perc_stud_debt_collect","perc_pop_nw","perc_no_bach","avg_income"]    
       }
 
